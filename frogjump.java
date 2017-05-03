1958fad5dab7befad7f891dac7d13003bdc0a64c
Chakrapani Kanamarlapudi <chakrapani.k@myntra.com>
2017-05-03 21:04:22 +0530

    frogjump code checkin

---
+++ frogjump.java
@@ -0,0 +1,36 @@
+// you can also use imports, for example:
+// import java.util.*;
+
+// you can write to stdout for debugging purposes, e.g.
+// System.out.println("this is a debug message");
+
+class FrogJump {
+    public int solution(int X, int[] A) {
+        // write your code in Java SE 8
+
+        int n = A.length;
+
+        if( n < X)
+            return -1;
+
+        int[] output = new int[X];
+
+        int check = 0;
+
+        for(int i=0; i< n; i++)
+        {
+            if(output[A[i]-1] == 0)
+            {
+                output[A[i]-1] = 1;
+               
+
+                check++;
+
+                if(check == output.length)
+                    return i;
+            }
+        }
+
+        return -1;
+    }
+}
